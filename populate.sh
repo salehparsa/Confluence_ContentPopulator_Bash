9fcb07f1600dd746d1e0a4f923409af2c51af869
Saleh <sparsa@atlassian.com>
2016-08-18 22:54:30 +0800

    validate number, regex for check URL

--- populate.sh
+++ populate.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 echo What is your Confluence URL?
 read URL
+URLregex='(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
 echo Please Enter Confluence User Name:
 read username
-if [ $# -eq 0 ]
-  then
-    echo "Please enter your username:"
-    exit 1
-fi
 echo Please Enter your password:
 unset password;
 while IFS= read -r -s -n1 pass; do
@@ -19,9 +15,11 @@ while IFS= read -r -s -n1 pass; do
      password+=$pass
   fi
 done
-echo Hello $username, how many pages do you want?
-read number
-echo username, what is your preference page prefix?
+while [[ ! $number =~ ^[0-9]+$ ]]; do
+  echo Hello $username, how many pages do you want?
+  read number
+done
+echo $username, what is your preference page prefix?
 read page_title
 for (( i = 0; i < $number; i++ )); do
 	curl -u $username:$password -X POST -H 'Content-Type: application/json' -d'{"type":"page","title":"'$page_title'_'$i'","space":{"key":"SAL"},"body":{"storage":{"value":"<p>This is a new page</p>","representation":"storage"}}}' $URL/rest/api/content/ | python -mjson.tool
@@ -29,4 +27,3 @@ done
 
 echo "Done"
 
-curl -u admin:admin -X POST -H http://localhost:8090/confluence/rest/api/content/ | python -mjson.tool
